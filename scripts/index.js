7a0fa5cf4cb4aa420e6ce2c18f75041174cdef91
Sergei Pradko <rodzy28@yandex.ru>
2025-04-12 14:57:12 +0300

    Сделал функции стелочными

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+const createCard = arrayCards => {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardTitle = card.querySelector('.card__title');
+  const cardImage = card.querySelector('.card__image');
+  const cardDeleteButton = card.querySelector('.card__delete-button');
+
+  cardTitle.textContent = arrayCards.name;
+  cardImage.src = arrayCards.link;
+  cardImage.alt = `На фото ${arrayCards.name}`;
+  
+  cardDeleteButton.addEventListener('click', deleteCard);
+
+  return card;
+}
 
 // @todo: Функция удаления карточки
+const deleteCard = (evt) => {
+  evt.target.closest('.card').remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+  cardList.append(createCard(item));
+});
