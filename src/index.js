2b9b321ad20c900192b1dac7f9cbf39dae5d4f72
Sergei Pradko <rodzy28@yandex.ru>
2025-05-04 17:09:25 +0300

    upd popup user

--- src/index.js
+++ src/index.js
@@ -5,13 +5,20 @@ import { openModal, closePopupButton } from "./components/modal.js";
 
 const popupEditUser = document.querySelector(".popup_type_edit");
 const popupAddCard = document.querySelector(".popup_type_new-card");
-const popupImage = document.querySelector(".popup_type_image");
+// const popupImage = document.querySelector(".popup_type_image");
 const profileButton = document.querySelector(".profile__edit-button");
 const cardButton = document.querySelector(".profile__add-button");
 const cardList = document.querySelector(".places__list");
+const formElementUser = document.forms["edit-profile"];
+const nameInput = formElementUser.elements.name;
+const jobInput = formElementUser.elements.description;
+const userName = document.querySelector(".profile__title");
+const userJob = document.querySelector(".profile__description");
 
 profileButton.addEventListener("click", () => {
   openModal(popupEditUser);
+  nameInput.value = userName.textContent;
+  jobInput.value = userJob.textContent;
   closePopupButton(popupEditUser);
 });
 
